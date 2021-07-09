e1e4204dd18f939b782bd368c0b93af36bfea05a
jeffstienstra <jeffstienstra@gmail.com>
2021-07-08 23:57:49 -0400

    journals show/remove actions are working. journal add and edit have buttons and send data to the console, but not fully functioning

--- src/views/Favorites.vue
+++ src/views/Favorites.vue
@@ -6,17 +6,17 @@
       <h3>{{ favorite.park_name }}</h3>
       <img v-bind:src="favorite.image_url" v-bind:key="favorite.id" alt="" style="width: 25%" />
       <div>
+        <button v-on:click="destroyFavorite(favorite)">Remove</button>
         <button v-on:click="favoriteShow(favorite)">Park Details</button>
-        <button v-on:click="showJournals()">Journals</button>
-        <button v-on:click="addJournal()">+Journal</button>
+        <button v-on:click="showJournals(favorite)">Field Notes</button>
+        <button v-on:click="addJournal(favorite)">+</button>
         <p>________________________________________</p>
       </div>
     </div>
 
-    <!-- modal -->
+    <!-- favorites modal -->
     <dialog id="park-details" style="width: 60%">
       <form method="dialog">
-        <button v-on:click="destroyPark(favorite)">Remove</button>
         <button float:right>Close</button>
         <!-- <button type="button" class="close" data-dismiss="modal">Close</button> -->
         <h1>{{ favorite.fullName }}</h1>
@@ -39,9 +39,30 @@
         <p>{{ favorite.directionsInfo }}</p>
 
         <h3>Entrance Fees</h3>
-        <p>{{ favorite.entranceFees }}</p>
+        <p>{{ entranceFee }}</p>
 
         <!-- <button>Close</button> -->
+      </form>
+    </dialog>
+
+    <!-- journals modal -->
+    <dialog id="journals" style="width: 60%">
+      <form method="dialog">
+        <div>
+          <!-- <input v-on:click="addJournal(favorite)" type="button" value="Add Field Note" /> -->
+          <button style="float: right">Close</button>
+        </div>
+
+        <div v-for="journal in journals" v-bind:key="journal.id">
+          <p>{{ journal.date }}</p>
+
+          <h1>{{ journal.title }}</h1>
+
+          <p>{{ journal.body }}</p>
+          <button v-on:click="destroyJournal(journal)">Delete</button>
+          <button v-on:click="editJournal(journal)">Edit</button>
+          <p>________________________________________</p>
+        </div>
       </form>
     </dialog>
   </div>
@@ -61,7 +82,8 @@ export default {
       myFavorites: {},
       favorite: {},
       park: {},
-      // parkCode: {},
+      journals: {},
+      entranceFee: {},
     };
   },
   created: function () {
@@ -77,8 +99,7 @@ export default {
     favoriteShow: function (favorite) {
       axios.get(`parks/${favorite.parkCode}`).then((response) => {
         console.log("This Favorite ->", response);
-        this.favorite = response.data[0]; // <-hard-coded for now. can't get park.parkCode to pass in params
-        // console.log("favorite->", this.favorite);
+        this.favorite = response.data[0];
 
         // get favorite's images
         var images = [];
@@ -98,10 +119,16 @@ export default {
         }
         this.currentActivities = activities.sort().join(", ");
 
+        // format entranceFee fee
+        console.log(this.favorite.entranceFees[0]);
+        var entranceFee = `$${this.favorite.entranceFees[0]["cost"]} 
+        ${this.favorite.entranceFees[0]["description"]}`;
+        this.entranceFee = entranceFee;
+
         document.querySelector("#park-details").showModal();
       });
     },
-    destroyPark: function (favorite) {
+    destroyFavorite: function (favorite) {
       console.log("destroy favorite", favorite);
       axios.delete(`/favorites/${favorite.parkCode}`).then((response) => {
         console.log("Destroy successful", response.data);
@@ -110,7 +137,29 @@ export default {
         // this.$router.push("/favorites");
       });
     },
-    addJournal: function () {},
+    showJournals: function (favorite) {
+      console.log(favorite["journals"]);
+      this.journals = favorite["journals"];
+      // axios.get(`journals/${journal.favorite_id}`).then((response) => {
+      //   console.log("This journal ->", response);
+      //   this.journal = response.data;
+      //   console.log(this.journal);
+      document.querySelector("#journals").showModal();
+      // });
+    },
+    destroyJournal: function (journal) {
+      console.log("destroy journal", journal);
+      axios.delete(`/journals/${journal.id}`).then((response) => {
+        console.log("Journal destroyed", response.data);
+        window.location.reload();
+      });
+    },
+    addJournal: function (favorite) {
+      console.log("add journal, favorite_id =", favorite.id);
+    },
+    editJournal: function (journal) {
+      console.log("edit journal, id = ", journal.id);
+    },
   },
 };
 </script>
